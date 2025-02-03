eb088361854333f00b4e3fcf29738edec02219a9
Hari <harrygajjar2004@gmail.com>
2025-02-03 18:15:50 +0530

    added full stop

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ const Home = () => {
             <span className="orange_gradient text-center"> AI-Powered Prompts</span>
         </h1>
         <p className="desc text-center">
-            Promptopia is an open-source AI Prompting tool for modern world to discover, create and share creative prompts
+            Promptopia is an open-source AI Prompting tool for modern world to discover, create and share creative prompts.
         </p>
         <Feed/>
     </section>
